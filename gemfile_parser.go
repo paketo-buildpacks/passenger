17cbbc48718a11349a0785bb635440c2717324f5
Tim Hitchener <thitchener@pivotal.io>
2020-08-03 15:52:09 -0400

    Detect implementation first pass

---
+++ gemfile_parser.go
@@ -0,0 +1,39 @@
+package passenger
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+)
+
+type GemfileParser struct{}
+
+func NewGemfileParser() GemfileParser {
+	return GemfileParser{}
+}
+
+func (p GemfileParser) Parse(path string) (bool, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return false, nil
+		}
+
+		return false, fmt.Errorf("failed to parse Gemfile: %w", err)
+	}
+	defer file.Close()
+
+	quotes := `["']`
+	passengerRe := regexp.MustCompile(fmt.Sprintf(`^gem %spassenger%s`, quotes, quotes))
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := []byte(scanner.Text())
+		if passengerRe.Match(line) {
+			return true, nil
+		}
+	}
+
+	return false, nil
+}
