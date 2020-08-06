a5883351eafa0bb19bef6ebb6801012c7eb6e341
Forest Eckhardt <feckhardt@pivotal.io>
2020-08-06 12:27:57 -0400

    Finish implementation
    Co-authored-by: Sophie Wigmore <swigmore@vmware.com>

--- run/main.go
+++ run/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"os"
 
+	"github.com/paketo-buildpacks/packit"
+	"github.com/paketo-buildpacks/packit/scribe"
+	"github.com/paketo-community/passenger"
+)
+
+func main() {
+	parser := passenger.NewGemfileParser()
+	logger := scribe.NewLogger(os.Stdout)
+	packit.Run(
+		passenger.Detect(parser),
+		passenger.Build(logger),
+	)
 }
