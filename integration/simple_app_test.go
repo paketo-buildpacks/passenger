ae9e244054c449184db740f9e45b4adb948113fa
Rob Dimsdale-Zucker <robert.dimsdale@gmail.com>
2022-05-11 11:03:03 -0400

    Add support for filesystem-based SBOM

--- integration/simple_app_test.go
+++ integration/simple_app_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 	"testing"
 
 	"github.com/paketo-buildpacks/occam"
@@ -25,6 +26,7 @@ func testSimpleApp(t *testing.T, context spec.G, it spec.S) {
 	it.Before(func() {
 		pack = occam.NewPack()
 		docker = occam.NewDocker()
+
 	})
 
 	context("when building a simple app", func() {
@@ -40,6 +42,9 @@ func testSimpleApp(t *testing.T, context spec.G, it spec.S) {
 			var err error
 			name, err = occam.RandomName()
 			Expect(err).NotTo(HaveOccurred())
+
+			source, err = occam.Source(filepath.Join("testdata", "passenger_sinatra"))
+			Expect(err).NotTo(HaveOccurred())
 		})
 
 		it.After(func() {
@@ -50,11 +55,9 @@ func testSimpleApp(t *testing.T, context spec.G, it spec.S) {
 		})
 
 		it("creates a working OCI image with a passenger start command", func() {
-			var err error
-			source, err = occam.Source(filepath.Join("testdata", "passenger_sinatra"))
-			Expect(err).NotTo(HaveOccurred())
-
 			var logs fmt.Stringer
+			var err error
+
 			image, logs, err = pack.WithNoColor().Build.
 				WithBuildpacks(
 					settings.Buildpacks.MRI.Online,
@@ -82,16 +85,85 @@ func testSimpleApp(t *testing.T, context spec.G, it spec.S) {
 				"  Executing build process",
 				"  Getting the layer associated with curl:",
 				"    /layers/paketo-buildpacks_passenger/curl",
-				"",
+			))
+			Expect(logs).To(ContainLines(
 				MatchRegexp(`    Installing cURL \d+\.\d+\.\d+`),
 				"    Installation path: /layers/paketo-buildpacks_passenger/curl",
 				MatchRegexp(`    Dependency URI\: https\:\/\/deps\.paketo\.io\/curl\/curl_\d+\.\d+\.\d+_linux_noarch_bionic_.*\.tgz`),
 				MatchRegexp(`      Completed in ([0-9]*(\.[0-9]*)?[a-z]+)+`),
-				"",
+			))
+			Expect(logs).To(ContainLines(
 				"  Assigning launch processes:",
 				"    web (default): bash -c bundle exec passenger start --port ${PORT:-3000}",
-				"",
 			))
+		})
+
+		context("validating SBOM", func() {
+			var (
+				sbomDir string
+			)
+
+			it.Before(func() {
+				var err error
+				sbomDir, err = os.MkdirTemp("", "sbom")
+				Expect(err).NotTo(HaveOccurred())
+				Expect(os.Chmod(sbomDir, os.ModePerm)).To(Succeed())
+			})
+
+			it.After(func() {
+				Expect(os.RemoveAll(sbomDir)).To(Succeed())
+			})
+
+			it("writes SBOM files to the layer and label metadata", func() {
+				var err error
+				var logs fmt.Stringer
+
+				image, logs, err = pack.WithNoColor().Build.
+					WithPullPolicy("never").
+					WithBuildpacks(
+						settings.Buildpacks.MRI.Online,
+						settings.Buildpacks.Bundler.Online,
+						settings.Buildpacks.BundleInstall.Online,
+						settings.Buildpacks.Passenger.Online,
+					).
+					WithEnv(map[string]string{
+						"BP_LOG_LEVEL": "DEBUG",
+					}).
+					WithSBOMOutputDir(sbomDir).
+					Execute(name, source)
+				Expect(err).ToNot(HaveOccurred(), logs.String)
+
+				container, err = docker.Container.Run.
+					WithEnv(map[string]string{"PORT": "9090"}).
+					WithPublish("9090").
+					WithPublishAll().
+					Execute(image.ID)
+				Expect(err).NotTo(HaveOccurred())
+
+				Eventually(container).Should(BeAvailable())
+				Eventually(container).Should(Serve(ContainSubstring("Hello world!")).OnPort(9090))
+
+				Expect(logs).To(ContainLines(
+					fmt.Sprintf("  Generating SBOM for /layers/%s/curl", strings.ReplaceAll(settings.Buildpack.ID, "/", "_")),
+					MatchRegexp(`      Completed in \d+(\.?\d+)*`),
+				))
+				Expect(logs).To(ContainLines(
+					"  Writing SBOM in the following format(s):",
+					"    application/vnd.cyclonedx+json",
+					"    application/spdx+json",
+					"    application/vnd.syft+json",
+				))
+
+				// check that all required SBOM files are present
+				Expect(filepath.Join(sbomDir, "sbom", "launch", strings.ReplaceAll(settings.Buildpack.ID, "/", "_"), "curl", "sbom.cdx.json")).To(BeARegularFile())
+				Expect(filepath.Join(sbomDir, "sbom", "launch", strings.ReplaceAll(settings.Buildpack.ID, "/", "_"), "curl", "sbom.spdx.json")).To(BeARegularFile())
+				Expect(filepath.Join(sbomDir, "sbom", "launch", strings.ReplaceAll(settings.Buildpack.ID, "/", "_"), "curl", "sbom.syft.json")).To(BeARegularFile())
+
+				// check an SBOM file to make sure it has an entry for curl
+				contents, err := os.ReadFile(filepath.Join(sbomDir, "sbom", "launch", strings.ReplaceAll(settings.Buildpack.ID, "/", "_"), "curl", "sbom.cdx.json"))
+				Expect(err).NotTo(HaveOccurred())
+				Expect(string(contents)).To(ContainSubstring(`"name": "cURL"`))
+			})
 		})
 	})
 }
--- run/main.go
+++ run/main.go
@@ -7,17 +7,26 @@ import (
 	"github.com/paketo-buildpacks/packit/v2/cargo"
 	"github.com/paketo-buildpacks/packit/v2/chronos"
 	"github.com/paketo-buildpacks/packit/v2/postal"
+	"github.com/paketo-buildpacks/packit/v2/sbom"
 	"github.com/paketo-buildpacks/packit/v2/scribe"
 	"github.com/paketo-buildpacks/passenger"
 )
 
+type Generator struct{}
+
+func (f Generator) GenerateFromDependency(dependency postal.Dependency, path string) (sbom.SBOM, error) {
+	return sbom.GenerateFromDependency(dependency, path)
+}
+
 func main() {
 	logger := scribe.NewEmitter(os.Stdout).WithLevel(os.Getenv("BP_LOG_LEVEL"))
+
 	packit.Run(
 		passenger.Detect(passenger.NewGemfileParser()),
 		passenger.Build(
 			postal.NewService(cargo.NewTransport()),
 			passenger.NewPassengerfileParser(),
+			Generator{},
 			chronos.DefaultClock,
 			logger,
 		),
